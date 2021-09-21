9371c71a4dd4f55559ea2aad79071bfda578899a
deepthi <deepthi.varanasi@iths.se>
2021-09-21 13:16:02 +0200

    final2

--- js/welcome.js
+++ js/welcome.js
@@ -1,10 +1,9 @@
-
 fetch("data/data1.json")
-.then(response => {
-    return response.json();
-})
-.then((data)=> {
-    console.log(data.sentence)
-    document.querySelector("#demo").innerHTML=data.sentence
+    .then(response => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data.sentence)
+        document.querySelector("#demo").innerHTML = data.sentence
 
-})
+    })
